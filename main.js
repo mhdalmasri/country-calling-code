e2a37ee4ff23770e36274ebd98aab0b34af8fbd0
Mohamad Almasri <44051605+mhdalmasri@users.noreply.github.com>
2019-03-05 11:37:50 +0100

    some beutify

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 function search() {
   const c = document.getElementById("code").value;
-  const url = `https://restcountries.eu/rest/v2/callingcode/${c}`
-  fetch(url)
+  fetch(`https://restcountries.eu/rest/v2/callingcode/${c}`)
     .then(response => response.json())
-    .then(data => {console.log(data)
-  let country = document.getElementById("js")
-  country.innerHTML = `<h1>${data[0].name} ==> ${data[0].alpha2Code}</h1>
-  <img style="width:100px" src="${data[0].flag}">
-  `})
-}
+    .then(data => { console.log(data)
+     document.getElementById("js").innerHTML = `<h1>${data[0].name} ==> ${data[0].alpha2Code}</h1>
+     <img style="width:100px" src="${data[0].flag}">`
+    })}
