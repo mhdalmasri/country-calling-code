b62a0384daf87b289bbcf916ba1b6e455181417a
Mohamad Almasri <44051605+mhdalmasri@users.noreply.github.com>
2019-03-05 11:49:28 +0100

    some changes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function search() {
-  const c = document.getElementById("code").value;
+  const c = document.querySelector("#code").value;
   fetch(`https://restcountries.eu/rest/v2/callingcode/${c}`)
     .then(response => response.json())
     .then(data => { console.log(data)
-     document.getElementById("js").innerHTML = `<h1>${data[0].name} ==> ${data[0].alpha2Code}</h1>
+     document.querySelector("#js").innerHTML = `<h1>${data[0].name} (${data[0].alpha2Code})</h1>
      <img style="width:100px" src="${data[0].flag}">`
     })}
